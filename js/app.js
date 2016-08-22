21adb8b32b106e0fa6821e8cadcc381125f4686d
root <sandeep.k@betsol.com>
2016-08-22 11:17:10 +0000

    Finished, Finally!

--- js/app.js
+++ js/app.js
@@ -34,6 +34,15 @@ app.controller('signupCtrl', ['$scope', '$location', '$http', function($scope, $
     });
     request.success(function(data){
       var response = angular.fromJson(data);    //data indicates 'body' of the postman app
+      if(!response["error"]) {
+        sessionStorage.userId = response["id"];
+        sessionStorage.email = response["email"];
+        sessionStorage.password = response["password"];
+        $location.path('/dashboard');
+      }
+      else {
+        $scope.responseMessage = response["message"][0];
+      }
       console.log(response);
     });
     request.error(function(data){
@@ -43,7 +52,118 @@ app.controller('signupCtrl', ['$scope', '$location', '$http', function($scope, $
 }]);
 
 app.controller('loginCtrl', ['$scope', '$location', '$http', '$cookies', function($scope, $location, $http, $cookies) {
+  this.loginData = {};
+  var request = null;
+  var now = new Date(),
+  expDate = new Date(now.getFullYear(), now.getMonth() +6, now.getDate());
+  if($cookies.get('email')) {
+   this.loginData.email = $cookies.get('email');
+   $scope.rememberMe = true;
+  }
+
+  $scope.login = function() {
+    var request = $http({
+      method: 'POST',
+      url: URL + "login",
+      data: this.loginData,
+      headers: {
+        "content-type":"application/json"
+      }
+    });
+    request.success(function(data){
+
+      var response = angular.fromJson(data);
+      if($scope.rememberMe) {
+            $cookies.put('email', this.loginData.email,{
+            expires: expDate
+          });
+      } else {
+          $scope.rememberMe = false;
+          $cookies.remove("email");
+      }
+      if(!response["error"]){
+        sessionStorage.userId = response["id"];
+        sessionStorage.email = response["email"];
+        sessionStorage.password = response["password"];
+        $location.path('/dashboard');
+      }
+      else {
+        $scope.responseMessage = response["message"][0];
+      }
+      console.log(response);
+    });
+    request.error(function(data){
+      console.log(data);
+    });
+
+  }
 }]);
 
 app.controller('dashboardCtrl', ['$scope', '$location', '$http', '$cookies', function($scope, $location, $http, $cookies) {
+    $scope.getProfileData = function() {
+       var request = $http({
+           method: "GET",
+           url: URL + "profile/" + sessionStorage.userId
+       });
+       request.success(function(data) {
+           $scope.profileData = angular.fromJson(data);
+       });
+       request.error(function(data) {
+           console.log(data);
+       });
+   }
+   $scope.getProfileData();
+   $scope.updateProfile = function() {
+       delete this.profileData["connectionRequests"];
+       delete this.profileData["connections"];
+       delete this.profileData["suggestions"];
+       var request = $http({
+           method: "PUT",
+           url: URL + "profile/" + sessionStorage.userId,
+           crossDomain: true,
+           data: this.profileData
+       });
+       request.success(function(data) {
+           $scope.responseMessage = "Update successful.";
+           $("#dashboardMsgModal").modal('show');
+           $scope.getProfileData();
+       });
+       request.error(function(data) {
+           console.log(data);
+       });
+   }
+   $scope.sendConnection = function(receiverId) {
+       var request = $http({
+           method: "POST",
+           crossDomain: true,
+           url: URL + "request/send/" + sessionStorage.userId + "/" + receiverId
+       });
+       request.success(function(data) {
+           $scope.responseMessage = "Your request has been sent.";
+           $("#dashboardMsgModal").modal('show');
+           $scope.getProfileData();
+       });
+       request.error(function(data) {
+           console.log(data);
+       });
+   }
+   $scope.acceptConnectionRequest = function(receiverId) {
+    var request = $http({
+          method: "POST",
+          crossDomain: true,
+          url: URL + "request/accept/" + receiverId
+      });
+      request.success(function(data) {
+          $scope.responseMessage = "Request accepted. You now have a new connection.";
+          $("#dashboardMsgModal").modal('show');
+          $scope.getProfileData();
+      });
+      request.error(function(data) {
+          console.log(data);
+      });
+ }
+ $scope.logout = function() {
+   sessionStorage.clear();
+   $location.path("/login");
+ }
 }]);
